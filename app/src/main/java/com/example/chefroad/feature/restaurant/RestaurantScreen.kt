2235751b077974d0a6e8bedf562f5850c4117c35
Gwon Hyoyun <gydbs3803@naver.com>
2024-12-06 05:09:05 +0900

    RestaurantScreen 생성 (식당 페이지)

---
+++ app/src/main/java/com/example/chefroad/feature/restaurant/RestaurantScreen.kt
@@ -0,0 +1,10 @@
+package com.example.chefroad.feature.restaurant
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun RestaurantScreen(navController: NavController) {
+    Text(text = "식당 페이지")
+}
